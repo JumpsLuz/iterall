5b84abe3d1c52cd4462b8effee2ad1cac9699547
JumpyLight <jvemillapele@outlook.com>
2026-01-14 13:26:28 -0300

    COMMIT DE PRUEBA #1.1 FIX
    Se arreglo error de duplicacion

--- public/iniciosesion_prueba.php
+++ public/iniciosesion_prueba.php
@@ -3,7 +3,7 @@
 <html>
     <body>
         <h2>Iniciar Sesión</h2>
-        <form action="procesar_registro.php" method="POST">
+        <form action="procesador.php?action=login" method="POST">
             <input type="email" name="email" placeholder="tucorreo@aqui.com" required><br><br>
             <input type="password" name="password" placeholder="Contraseña" required><br><br>
             <button type="submit">Iniciar Sesión</button>
